b3b66bef265c74b7f1222f4826020869d9703501
Dmitriy <dimka_it@list.ru>
2022-06-14 19:17:52 +0300

    changed NavBar, added NavBar Profile

--- src/components/ui/navBar.jsx
+++ src/components/ui/navBar.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import NavProfile from "./navProfile";
 
 const NavBar = () => {
-  return (
-    <ul className={"nav"}>
-      <li className={"nav-link"}><Link style={classes.link} to={"/"}>Main</Link></li>
-      <li className={"nav-link"}><Link style={classes.link} to={"/login"}>Login</Link></li>
-      <li className={"nav-link"}><Link style={classes.link} to={"/users"}>Users</Link></li>
-    </ul>
-  );
-};
+  const { currentUser } = useAuth();
 
-const classes = {
-  link: {
-    textDecoration: "none",
-    fontWeight: 500,
-    fontSize: 18
-  }
+  return (
+    <nav className={"navbar bg-light mb-3"}>
+      <div className={"container-fluid"}>
+        <ul className={"nav"}>
+          <li className={"nav-item"}><Link className={"nav-link"} to={"/"}>Main</Link></li>
+          {currentUser && (
+            <li className={"nav-item"}><Link className={"nav-link"} to={"/users"}>Users</Link></li>
+          )}
+        </ul>
+        <div className={"d-flex"}>
+          {currentUser ? <NavProfile /> : <Link className={"nav-link"} to={"/login"}>Login</Link>}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
