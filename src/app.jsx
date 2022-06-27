761f04234773adf724017990522e07c56e41f7c0
Dmitriy <dimka_it@list.ru>
2022-06-27 22:05:24 +0300

    create qualitiesReducer

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -12,8 +12,17 @@ import QualitiesProvider from "./hooks/useQualities";
 import AuthProvider from "./hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logOut";
+import { useDispatch } from "react-redux";
+import { loadQualitiesList } from "./store/qualities";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log("wow");
+    dispatch(loadQualitiesList());
+  }, []);
+
   return (
     <>
       <AuthProvider>
--- src/store/qualities.js
+++ src/store/qualities.js
@@ -1,13 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
+import qualityService from "../services/quality.service";
 
 const qualitiesSlice = createSlice({
   name: "qualities",
   initialState: {
     entities: null,
-    isLoading: true
+    isLoading: true,
+    error: null
+  },
+  reducers: {
+    qualitiesRequested: (state) => {
+      state.isLoading = true;
+    },
+    qualitiesReceved: (state, action) => {
+      state = action.payload;
+      state.isLoading = false;
+    },
+    qualitiesRequestFiled: (state, action) => {
+      state.error = action.payload;
+      state.isLoading = false;
+    }
   }
 });
 
-const { reducer: qualitiesReducer } = qualitiesSlice;
+const { reducer: qualitiesReducer, actions } = qualitiesSlice;
+const { qualitiesRequested, qualitiesReceved, qualitiesRequestFiled } = actions;
+
+export const loadQualitiesList = () => async (dispatch) => {
+  dispatch(qualitiesRequested());
+  try {
+    const { content } = await qualityService.fetchAll();
+    dispatch(qualitiesReceved(content));
+  } catch (error) {
+    dispatch(qualitiesRequestFiled(error.message));
+  }
+};
 
 export default qualitiesReducer;
