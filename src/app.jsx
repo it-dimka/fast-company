907fd815188101fdf2c0403ed90e2574165d6db8
Dmitriy <dimka_it@list.ru>
2022-07-01 19:02:28 +0300

    remove Hooks

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,6 @@ import Login from "./layouts/login";
 import Users from "./layouts/users";
 import NotFound from "./layouts/notFound";
 import NavBar from "./components/ui/navBar";
-import AuthProvider from "./hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logOut";
 import AppLoader from "./components/ui/hoc/appLoader";
@@ -16,17 +15,15 @@ const App = () => {
   return (
     <>
       <AppLoader>
-        <AuthProvider>
-          <NavBar />
-          <Switch>
-            <ProtectedRoute path={"/users/:userId?/:edit?"} component={Users} />
-            <Route path={"/login/:type?"} component={Login} />
-            <Route path={"/logout"} component={LogOut} />
-            <Route exact path={"/"} component={Main} />
-            <Route path={"/404"} component={NotFound} />
-            <Redirect to={"/404"} />
-          </Switch>
-        </AuthProvider>
+        <NavBar />
+        <Switch>
+          <ProtectedRoute path={"/users/:userId?/:edit?"} component={Users} />
+          <Route path={"/login/:type?"} component={Login} />
+          <Route path={"/logout"} component={LogOut} />
+          <Route exact path={"/"} component={Main} />
+          <Route path={"/404"} component={NotFound} />
+          <Redirect to={"/404"} />
+        </Switch>
       </AppLoader>
       <ToastContainer />
     </>
